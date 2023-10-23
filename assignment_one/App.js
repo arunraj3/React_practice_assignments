325b2668542fe86c0f724ece09e11edbdf661ede
arunraj3 <arunrajgovindan2002@gmail.com>
2023-10-23 17:08:20 +0530

    Assignment one

--- assignment_one/App.js
+++ assignment_one/App.js
@@ -1,5 +1,4 @@
-
-// 2. Hello world using Javascript DOM manipulation 
+// 2. Hello world using Javascript DOM manipulation
 // const headingOne = document.createElement("h1");
 
 // headingOne.innerHTML = "Hello World using Javascript";
@@ -13,3 +12,21 @@
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 
 // root.render(headingTwo);
+
+// creating nested Elements
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   React.createElement(
+//     "div",
+//     {},
+//     React.createElement(
+//       "div",
+//       { id: "parent" },
+//       React.createElement(
+//         "div",
+//         { id: "children" },
+//         "Nested Elements using React Create Element"
+//       )
+//     )
+//   )
+// );
