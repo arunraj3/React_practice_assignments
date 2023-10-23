9311945b6e3acf77a8f058219ced7493ae093ebc
arunraj3 <arunrajgovindan2002@gmail.com>
2023-10-23 14:59:40 +0530

    Code assignment

---
+++ assignment_one/App.js
@@ -0,0 +1,15 @@
+
+// 2. Hello world using Javascript DOM manipulation 
+// const headingOne = document.createElement("h1");
+
+// headingOne.innerHTML = "Hello World using Javascript";
+
+// document.getElementById("root").append(headingOne);
+
+// 3. Hello world using React
+
+// const headingTwo = React.createElement("h1",{},"Hello world using React");
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(headingTwo);
