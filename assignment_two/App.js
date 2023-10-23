d79d1e7601803da3d470130f6c4b9d1a61318e97
arunraj3 <arunrajgovindan2002@gmail.com>
2023-10-23 19:10:52 +0530

    QA-1

---
+++ assignment_two/App.js
@@ -0,0 +1,10 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const headingElementUsingJSX =  <h1>Hello world using JSX</h1>;
+
+root.render(headingElementUsingJSX);
